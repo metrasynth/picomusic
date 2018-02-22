5fcb6e1d0a126d1596adb4474c5f9d6e3c8578fe
Matthew Scott <matt@11craft.com>
2018-02-21 20:03:15 -0600

    Don't try to import plantweb; we don't use it in our docs yet

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'plantweb.directive',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

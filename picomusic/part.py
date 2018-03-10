7211ecb291bd52d75635a8aa8f80721aa2313e67
Matthew Scott <matt@11craft.com>
2018-03-10 11:26:53 -0600

    Add repr to Part

--- picomusic/part.py
+++ picomusic/part.py
@@ -3,3 +3,9 @@ class Part:
     def __init__(self, voice=None, name=None):
         self.voice = voice
         self.name = name
+
+    def __repr__(self):
+        if self.name:
+            return f'<Part name={repr(self.name)} voice={repr(self.voice)}>'
+        else:
+            return f'<Part id={hex(id(self))} voice={repr(self.voice)}>'

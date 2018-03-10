62bb94ef5f3f8ac6dd3199589165bca5bcd400cf
Matthew Scott <matt@11craft.com>
2018-03-10 11:28:34 -0600

    Add play(), stop(), playing, and stopped to Stage

--- picomusic/stage.py
+++ picomusic/stage.py
@@ -1,16 +1,46 @@
+from rv.api import NOTECMD, Project
+
 from .sunvoxproject import project_from_timeline
 from .timeline import Timeline
 
 
 class Stage:
-    def __init__(self, sunvox):
+    def __init__(self, sunvox) -> None:
         self.slot = sunvox.Slot()
         self.timeline = Timeline()
 
-    def play_once(self):
+    def play_once(self) -> None:
+        self.stop(reset=True)
         self.slot.load(self.sunvox_project)
-        self.slot.play_from_beginning()
         self.slot.set_autostop(True)
+        self.slot.play_from_beginning()
+
+    def play(self) -> None:
+        """Play the performance in a continuous loop."""
+        self.stop(reset=True)
+        self.slot.load(self.sunvox_project)
+        self.slot.set_autostop(False)
+        self.slot.play_from_beginning()
+
+    def stop(self, reset=False) -> None:
+        """
+        Stop the performance, optionally resetting audio engine.
+        :param reset: Forcibly reset audio engine if True.
+        """
+        reset = reset or self.stopped
+        self.slot.stop()
+        if reset:
+            self.slot.send_event(0, NOTECMD.CLEAN_SYNTHS, 0, 0, 0, 0)
+
+    @property
+    def playing(self) -> bool:
+        """Is the performance playing?"""
+        return not self.stopped
+
+    @property
+    def stopped(self) -> bool:
+        """Is the performance stopped?"""
+        return self.slot.end_of_song()
 
     @property
     def sunvox_project(self):
